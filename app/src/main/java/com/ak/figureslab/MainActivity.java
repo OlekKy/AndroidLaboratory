5aab40677ccfcc9ef72296750b4d9a523c781090
ak <a1795k@gmail.com>
2018-11-03 20:02:09 +0100

    Add onClick & onLongClick listener to table rows. Add PopupMenu. Add Duplicate & Remove element from list implementation. Save data when screen rotate. Fix table_lay.xml.

--- app/src/main/java/com/ak/figureslab/MainActivity.java
+++ app/src/main/java/com/ak/figureslab/MainActivity.java
@@ -2,17 +2,23 @@ package com.ak.figureslab;
 
 import android.content.Intent;
 import android.os.Build;
+import android.os.Parcelable;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.PopupMenu;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
@@ -21,6 +27,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     public static LinkedList<Figure> figureList;
+   // public static ArrayList<Figure> figureList2;
     public static LinkedList<TableRow> rowsList;
 
     public static TableLayout mainLayout;
@@ -33,16 +40,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.table_lay);
 
         mainLayout = findViewById(R.id.tl);
+        mainLayout.setColumnStretchable(0,true);
+        mainLayout.setColumnStretchable(1,true);
+        mainLayout.setColumnStretchable(2,true);
 
         figureList = generateFigures(100,0,1);
         showTable(figureList);
-
-//        tableRow.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                tableRow.setBackgroundColor(-65536);
-//            }
-//        });
 
         Button goSettings = findViewById(R.id.button4);
         goSettings.setOnClickListener(new View.OnClickListener() {
@@ -199,15 +202,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public static void showTable(LinkedList<Figure> figList) {
+    public static void showTable(final LinkedList<Figure> figList) {
         mainLayout.removeAllViews();
         rowsList = new LinkedList<>();
         for (int i = 0; i < figList.size(); i++) {
-            tableRow = new TableRow(mainLayout.getContext()); // WORKING
+            tableRow = new TableRow(mainLayout.getContext());
 
             tableRow.setLayoutParams(new LayoutParams(
                     LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-
 
             if (figList.get(i).type() == R.drawable.square) {
                 Square square = (Square) figList.get(i);
@@ -218,11 +220,14 @@ public class MainActivity extends AppCompatActivity {
                 tableRow.addView(imgView);
 
                 TextView txtView1 = new TextView(mainLayout.getContext());
-                String fieldValue = String.format("                   %.3f            ", square.field());
+                txtView1.setGravity(Gravity.CENTER);
+
+                String fieldValue = String.format("%.3f", square.field());
                 txtView1.setText(fieldValue);
                 tableRow.addView(txtView1);
 
                 TextView txtView2 = new TextView(mainLayout.getContext());
+                txtView2.setGravity(Gravity.CENTER);
                 String attrValue = String.format("Przekatna \n %.3f", square.lengthParam());
                 txtView2.setText(attrValue);
                 tableRow.addView(txtView2);
@@ -235,16 +240,18 @@ public class MainActivity extends AppCompatActivity {
                 tableRow.addView(imgView);
 
                 TextView txtView1 = new TextView(mainLayout.getContext());
-                String fieldValue = String.format("                   %.3f            ", triangle.field());
+                txtView1.setGravity(Gravity.CENTER);
+
+                String fieldValue = String.format("%.3f", triangle.field());
                 txtView1.setText(fieldValue);
                 tableRow.addView(txtView1);
 
                 TextView txtView2 = new TextView(mainLayout.getContext());
+                txtView2.setGravity(Gravity.CENTER);
                 String attrValue = String.format("Wysokosc \n %.3f", triangle.lengthParam());
                 txtView2.setText(attrValue);
                 tableRow.addView(txtView2);
 
-                //mainLayout.addView(tableRow);
             } else {
                 Circle circle = (Circle) figList.get(i);
                 ImageView imgView = new ImageView(mainLayout.getContext());
@@ -252,11 +259,14 @@ public class MainActivity extends AppCompatActivity {
                 tableRow.addView(imgView);
 
                 TextView txtView1 = new TextView(mainLayout.getContext());
-                String fieldValue = String.format("                   %.3f            ", circle.field());
+                txtView1.setGravity(Gravity.CENTER);
+
+                String fieldValue = String.format("%.3f", circle.field());
                 txtView1.setText(fieldValue);
                 tableRow.addView(txtView1);
 
                 TextView txtView2 = new TextView(mainLayout.getContext());
+                txtView2.setGravity(Gravity.CENTER);
                 String attrValue = String.format("Srednica \n %.3f", circle.lengthParam());
                 txtView2.setText(attrValue);
                 tableRow.addView(txtView2);
@@ -265,8 +275,7 @@ public class MainActivity extends AppCompatActivity {
             rowsList.add(tableRow);
             mainLayout.addView(tableRow);
         }
-        for ( int x = 0 ; x < rowsList.size() ; x++)
-            System.out.println(rowsList.get(x) + " : " + x);
+
         for ( int y = 0 ; y < rowsList.size() ; y++){
             final int finalY = y;
             rowsList.get(y).setOnClickListener(new View.OnClickListener() {
@@ -279,6 +288,36 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+            rowsList.get(y).setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    for (int z = 0 ; z < rowsList.size(); z ++){
+                        rowsList.get(z).setBackgroundColor(0);
+                    }
+                    rowsList.get(finalY).setBackgroundColor(-256);
+                    PopupMenu popupMenu = new PopupMenu(mainLayout.getContext(), v);
+                    popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            if (item.getTitle().equals("Duplikuj")){
+                                figureList.add(finalY+1, figureList.get(finalY));
+                                showTable(figureList);
+                                //Toast.makeText(mainLayout.getContext(), "Duplicate" +figureList.get(finalY).field(), Toast.LENGTH_LONG).show();
+                            } else if (item.getTitle().equals("Usuń")){
+                                figureList.remove(finalY);
+                                showTable(figureList);
+                                //Toast.makeText(mainLayout.getContext(), "Deleted", Toast.LENGTH_LONG).show();
+                            }
+                            return true;
+                        }
+                    });
+
+                    popupMenu.show();
+                    return true;
+                }
+            });
         }
     }
 
@@ -304,13 +343,14 @@ public class MainActivity extends AppCompatActivity {
                 figures.add(new Circle(los[i][1]));
             }
         }
+        figureList = figures;
         return figures;
     }
 
     public static int countCircles(){
         int number = 0;
         for (int i = 0 ; i < figureList.size() ; i++ ) {
-            if (figureList.get(i).name() == "KOLO"){
+            if (figureList.get(i).name().equals("KOLO")){
                 number++;
             }
         }
@@ -319,7 +359,7 @@ public class MainActivity extends AppCompatActivity {
     public static int countSquares(){
         int number = 0;
         for (int i = 0 ; i < figureList.size() ; i++ ) {
-            if (figureList.get(i).name() == "KWADRAT"){
+            if (figureList.get(i).name().equals("KWADRAT")){
                 number++;
             }
         }
@@ -328,7 +368,7 @@ public class MainActivity extends AppCompatActivity {
     public static int countTriangles(){
         int number = 0;
         for (int i = 0 ; i < figureList.size() ; i++ ) {
-            if (figureList.get(i).name() == "TROJKAT"){
+            if (figureList.get(i).name().equals("TROJKAT")){
                 number++;
             }
         }
@@ -337,7 +377,7 @@ public class MainActivity extends AppCompatActivity {
     public static double fieldOfAllCircles(){
         double number = 0;
         for (int i = 0 ; i < figureList.size() ; i++){
-            if (figureList.get(i).name() == "KOLO"){
+            if (figureList.get(i).name().equals("KOLO")){
                 number = number + figureList.get(i).field();
             }
         }
@@ -346,7 +386,7 @@ public class MainActivity extends AppCompatActivity {
     public static double fieldOfAllSquares(){
         double number = 0;
         for (int i = 0 ; i < figureList.size() ; i++){
-            if (figureList.get(i).name() == "KWADRAT"){
+            if (figureList.get(i).name().equals("KWADRAT")){
                 number = number + figureList.get(i).field();
             }
         }
@@ -355,7 +395,7 @@ public class MainActivity extends AppCompatActivity {
     public static double fieldOfAllTriangles(){
         double number = 0;
         for (int i = 0 ; i < figureList.size() ; i++){
-            if (figureList.get(i).name() == "TROJKAT"){
+            if (figureList.get(i).name().equals("TROJKAT")){
                 number = number + figureList.get(i).field();
             }
         }
@@ -364,7 +404,7 @@ public class MainActivity extends AppCompatActivity {
     public static double paramsOfAllCircles(){
         double number = 0;
         for (int i = 0 ; i < figureList.size() ; i++){
-            if (figureList.get(i).name() == "KOLO"){
+            if (figureList.get(i).name().equals("KOLO")){
                 number = number + figureList.get(i).lengthParam();
             }
         }
@@ -373,17 +413,16 @@ public class MainActivity extends AppCompatActivity {
     public static double paramsOfAllSquares(){
         double number = 0;
         for (int i = 0 ; i < figureList.size() ; i++){
-            if (figureList.get(i).name() == "KWADRAT"){
+            if (figureList.get(i).name().equals("KWADRAT")){
                 number = number + figureList.get(i).lengthParam();
             }
         }
         return number;
     }
-
     public static double paramsOfAllTriangles(){
         double number = 0;
         for (int i = 0 ; i < figureList.size() ; i++){
-            if (figureList.get(i).name() == "TROJKAT"){
+            if (figureList.get(i).name().equals("TROJKAT")){
                 number = number + figureList.get(i).lengthParam();
             }
         }
--- app/src/main/java/com/ak/figureslab/Settings.java
+++ app/src/main/java/com/ak/figureslab/Settings.java
@@ -22,13 +22,17 @@ public class Settings extends AppCompatActivity {
         generateFigures.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int x = Integer.valueOf(editFiguresNr.getText().toString());
-                float min = Float.valueOf(editFiguresMin.getText().toString());
-                float max = Float.valueOf(editFiguresMax.getText().toString());
-                if(( x > 0 ) & ( x < 200 ) & ( max >= min ) & ( min > 0.1 ) & ( min < 10 ) & (max > 0.1 ) & ( max < 10)){
-                    MainActivity.showTable(MainActivity.generateFigures(x,min,max));
-                    finish();
-                } else Toast.makeText(v.getContext(), " PODAJ POPRAWNE WARTOSCI!", Toast.LENGTH_LONG).show();
+                if ((editFiguresNr.getText().toString().trim().length() > 0) & (editFiguresMin.getText().toString().trim().length() > 0)
+                        & (editFiguresMax.getText().toString().trim().length() > 0)){
+                    int x = Integer.valueOf(editFiguresNr.getText().toString());
+                    float min = Float.valueOf(editFiguresMin.getText().toString());
+                    float max = Float.valueOf(editFiguresMax.getText().toString());
+                    if(( x > 0 ) & ( x < 200 ) & ( max >= min ) & ( min > 0.1 ) & ( min < 10 ) & (max > 0.1 ) & ( max < 10)){
+                        MainActivity.showTable(MainActivity.generateFigures(x,min,max));
+                        finish();
+                    } else Toast.makeText(v.getContext(), " PODAJ POPRAWNE WARTOŚCI!", Toast.LENGTH_LONG).show();
+                } else  Toast.makeText(v.getContext(), " UZUPEŁNIJ WSZYSTKIE WARTOŚCI!", Toast.LENGTH_LONG).show();
+
 
             }
         });
